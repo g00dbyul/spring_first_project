a6cba377b0706fa16cc4f187ec8a1a452eb5c8e4
g00dbyul <skypower4203@gmail.com>
2025-02-01 17:41:22 +0900

    definition secure config for login

---
+++ src/main/java/com/godbyul/first_project/configurations/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.godbyul.first_project.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize ->
+                authorize.requestMatchers("/api/v1/sign/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .oauth2Login(oAuth -> oAuth
+                        .loginPage("/api/v1/sign/in")
+//                        .successHandler()
+//                        .failureHandler()
+//                        .userInfoEndpoint()
+                )
+                .formLogin(login -> login.loginPage("/api/v1/sign/in"))
+                .logout(logout -> logout.permitAll())
+                .csrf(Customizer.withDefaults());
+        return http.build();
+    }
+}
